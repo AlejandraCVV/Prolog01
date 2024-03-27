2db067732dc67709c346fb36cc58fab6266d5c5a
AlejandraCVV <lpze.alejandracristal.vargas.ve@unifranz.edu.bo>
2024-03-26 20:55:14 -0400

    26 de marzo

--- examen_en_aula.pl
+++ examen_en_aula.pl
@@ -61,3 +61,29 @@ enCategoria(computadora,producto).
 
 dentroCategoria(Producto,Categoria):-esProducto(Producto),esCategoria(Categoria),enCategoria(Producto,Categoria).
 estaDentroCategoria(Producto,Caracteristica):-enCategoria(Categoria2,Categoria),enCategoria(Producto,Categoria2).
+
+caracteristica('LG_510',blanco,barato,bueno).
+caracteristica('Samsung B125',negro,caro,malo).
+caracteristica('LG Batimix',rojo,blanco,malo).
+caracteristica('Samsung L 200',negro,caro,bueno).
+caracteristica('Bose',blanco,caro,malo).
+caracteristica('LG',rojo,barato,malo).
+caracteristica('XBox 360',verde,barato,malo).
+caracteristica('XBox one',verde,caro,bueno).
+caracteristica('PS 2',negro,barato,bueno).
+caracteristica('PS 3',negro,barato,bueno).
+caracteristica('Omen 560',plateado,barato,malo).
+caracteristica('Pavillon 15a',plateado,caro,bueno).
+caracteristica('Destruinator 500',plateado,barato,bueno).
+
+gustoMaria1(Producto):-esProducto(Producto),caracteristica(Producto,verde,_,_),enCategoria(Producto,producto).
+gustoMaria2(Producto):-esProducto(Producto),caracteristica(Producto,blanco,barato,_),enCategoria(Producto,entretenimiento).
+gustoMaria3(Producto):-esProducto(Producto),caracteristica(Producto,rojo,_,malo),enCategoria(Producto,electrodomestico).
+gustoMaria4(Producto):-esProducto(Producto),caracteristica(Producto,_,caro,_),enCategoria(Producto,producto).
+
+gustoJuana1(Producto):-esProducto(Producto),caracteristica(Producto,negro,caro,_),enCategoria(Producto,entretenimiento).
+gustoJuana2(Producto):-esProducto(Producto),caracteristica(Producto,plateado,_,bueno),enCategoria(Producto,computadora).
+gustoJuana3(Producto):-esProducto(Producto),caracteristica(Producto,_,barato,_),enCategoria(Producto,producto).
+
+gustoJeremias1(Producto):-esProducto(Producto),caracteristica(Producto,_,caro,bueno),enCategoria(Producto,electrodomestico).
+gustoJeremias2(Producto):-esProducto(Producto),caracteristica(Producto,_,barato,bueno),enCategoria(Producto,producto).
